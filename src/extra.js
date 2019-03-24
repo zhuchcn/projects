20a2eebb77e6b6626c8262165b0010d69031bc81
zhuchcn <zhuchcn@gmail.com>
2019-03-23 19:28:01 -0700

    1. fix sidebar menu at small screen 2. change logo

--- src/extra.js
+++ src/extra.js
@@ -1,20 +1,27 @@
 // The sidebar menu of mkdocs is a little crazy. Instead of create each separate sidebar menu for each top navbar item, it renders a whole sidebar menu, and sets the list items that do not belong to the current top navbar item as "visibility: hidden". So I have to change the format according to the router url
-if(window.location.pathname.search("post") === -1){
-    const menuItems = document.querySelectorAll(".md-sidebar--primary ul.md-nav__list + li.md-nav__item--nested")
-    for(i=0; i<menuItems.length; i++){
-        menuItems[i].style.display = "none"
-    }
-} else {
-    const items = document.querySelector(".md-sidebar--primary ul.md-nav__list").children
-    for(i=0; i < items.length; i++){
-        if(items[i].classList.contains("md-nav__item--active") !== true){
-            items[i].style.display = "none"
-        } else {
-            const fas = items[i].querySelectorAll("i.fa")
-            for(j=0; j < fas.length; j++){
-                fas[j].className = 'fa fa-caret-right' 
+if(window.innerWidth > 1341){
+    if(window.location.pathname.search("post") === -1){
+        const menuItems = document.querySelectorAll(".md-sidebar--primary ul.md-nav__list + li.md-nav__item--nested")
+        for(i=0; i<menuItems.length; i++){
+            menuItems[i].style.display = "none"
+        }
+    } else {
+        const items = document.querySelector(".md-sidebar--primary ul.md-nav__list").children
+        for(i=0; i < items.length; i++){
+            if(items[i].classList.contains("md-nav__item--active") !== true){
+                items[i].style.display = "none"
+            } else {
+                const fas = items[i].querySelectorAll("i.fa")
+                for(j=0; j < fas.length; j++){
+                    fas[j].className = 'fa fa-caret-right' 
+                }
             }
         }
+    }
+} else {
+    const items = document.querySelectorAll(".md-sidebar--primary li.md-nav__item--nested .fa")
+    for(i=0; i<items.length; i++){
+        items[i].className = 'fa fa-caret-right'
     }
 }
 
