913bc1fc6df8b22ea92c6abce2a0f0f3afd80b6f
Chenghao Zhu <chenghaozhu@campus-058-168.ucdavis.edu>
2019-07-07 16:46:45 -0700

    add new post 2019-07-07

--- src/extra.js
+++ src/extra.js
@@ -1,4 +1,7 @@
-// The sidebar menu of mkdocs is a little crazy. Instead of create each separate sidebar menu for each top navbar item, it renders a whole sidebar menu, and sets the list items that do not belong to the current top navbar item as "visibility: hidden". So I have to change the format according to the router url
+// The sidebar menu of mkdocs is a little crazy. Instead of create each separate
+// sidebar menu for each top navbar item, it renders a whole sidebar menu, and 
+// sets the list items that do not belong to the current top navbar item as 
+// "visibility: hidden". So I have to change the format according to the router url
 if(window.innerWidth > 1220){
     if(window.location.pathname.search("post") === -1){
         const menuItems = document.querySelectorAll(".md-sidebar--primary ul.md-nav__list + li.md-nav__item--nested")
@@ -11,7 +14,7 @@ if(window.innerWidth > 1220){
             if(items[i].classList.contains("md-nav__item--active") !== true){
                 items[i].style.display = "none"
             } else {
-                const fas = items[i].querySelectorAll("i.fa")
+                const fas = items[i].querySelectorAll("i")
                 for(j=0; j < fas.length; j++){
                     fas[j].className = 'fa fa-caret-right' 
                 }
@@ -29,5 +32,4 @@ if(window.innerWidth > 1220){
 const date = new Date()
 const year = date.getFullYear()
 const footer = document.querySelector(".md-footer-copyright__highlight")
-console.log(footer.innerText)
 footer.innerText = footer.innerText.replace('$YEAR', year)
